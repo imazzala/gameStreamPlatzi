704518040f8b4c320f8c602622cff8ee53be629e
imazzala <ivanmazzalay@gmail.com>
2021-10-20 13:59:52 -0300

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Ivan Ezequiel Mazzalay on 20/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
